cb8485028d00518362dc0b7c7839c55c465a3779
Rammar515 <rammarad515@gmail.com>
2015-01-14 10:25:32 +0100

    p760: remove media.stagefright.use-awesome

--- common.mk
+++ common.mk
@@ -203,8 +203,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Use awesome player for now
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.media.use-awesome=true \
-    media.stagefright.use-awesome=true
+    persist.sys.media.use-awesome=true
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
